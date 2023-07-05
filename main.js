9a75d6ed71c31c535cc1b7fed9fe0b2e171594ea
Meghan Carter <msc2010@yahoo.com>
2023-07-05 17:34:46 -0500

    timer

--- main.js
+++ main.js
@@ -2,26 +2,4 @@
 
 //one array iteration(map,forEach,filter,etc.)
 
-const image = document.createElement("https://static.vecteezy.com/system/resources/previews/008/856/128/non_2x/black-silhouette-of-a-man-in-a-pose-for-meditation-meditation-and-yoga-in-the-lotus-position-illustration-isolated-on-white-background-vector.jpg")
-image.src = meditationPose
 
-const emotionFaces = {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-        body: JSON.stringify()
-    }
-}
-
-function emotionFaces () {
-    const emotions = fetch("http://localhost:3000/emotions")
-    .then((response)=>response.json())
-    .then((json)=>renderFaces(json))
-}
-
-function renderFaces(emotions) {
-    document.addEventListener('DOMContentLoaded', function() {
-        emotionFaces()
-    })
-}
