cb6563b16ac8209f2463bd7c1efbb355e9a5c238
Meghan Carter <msc2010@yahoo.com>
2023-07-08 06:53:43 -0500

    update html

--- main.js
+++ main.js
@@ -2,4 +2,12 @@
 
 //one array iteration(map,forEach,filter,etc.)
 
-
+function meditationTimer() {
+    setInterval(function() {
+               date = new Date()
+               let hour = date.getHours();
+               let minutes = date.getMinutes();
+               let seconds = date.getSeconds();
+               document.getElementById("demo").innerHTML = hour + ":"+ minutes + ":" + seconds;
+  }, 1000);
+}
