ecb3b1c3f7186c65dca7b4292809a8536ad24ae6
Meghan Carter <msc2010@yahoo.com>
2023-07-21 13:32:50 -0500

    fetch

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 Phase 1 Project
 
 <!-- Should include project name, description, installation instructions, brief description of how to use your app, a Contributor's Guide, and a License (https://www.makeareadme.com/Links to an external site.) -->
+
+Welcome to Eudemonia! As you may know, taking proper care of your mental health has become an important topic within the last few years. Eudemonia is a mental health daily check-in that asks you a few simple questions to help you keep track of your daily emotions. Doing so allows you to become more aware of how you feel which helps improve your mental health.
+
+
+
+
+
+License
+
+[MIT](https://choosealicense.com/licenses/mit/)
--- main.js
+++ main.js
@@ -1,85 +1,39 @@
 //two event listeners w/ unique callback function
 
-//one array iteration(map,forEach,filter,etc.)
+//Your project must implement at least one instance of array iteration using available array methods (map, forEach, filter, etc). Interacting with your API data in some way should present an opportunity to use array iteration.
 
-fetch(`https://phase-1-project-usyy.onrender.com`), {
-  method:'POST',
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify ({
-      name: 'emotions'
-  })
-}
-.then(function(response){
-  return response.json();})
-  .then(function(data){
-    //data from DOM
-})
+fetch("http://localhost:3000/emotions")
+  .then (response => response.json())
+  .then (function(data){
+    console.log(data)
+  }) 
+//   method:'POST',
+//   headers: {
+//     'Content-Type': 'application/json'
+//   },
+//   body: JSON.stringify ({
+//       name: 'emotions'
+//   })
+// }
+// .then(function(response){
+//   return response.json();})
+//   .then(function(data){
+//     //data from DOM
+// })
 
 
 //submit forms
 
-let form = document.getElementById('form')
+let submitForm = document.querySelector("submitForm");
+let submitButton = document.querySelector("SubmitButton");
 
-form.addEventListener('submit',function(event){
-  event.preventDefault()
+submitButton.addEventListener("click", function(e){
+  e.preventDefault();
+  alert ("SUBMITTED!");
+});
+
+submitForm.addEventListener("submit", function(e) {
+  e.preventDefault();
 })
 
-//timer
 
-"use strict";
-
-let hour = 0;
-let minute = 0;
-let second = 0;
-let millisecond = 0;
-
-let cron;
-
-document.form_main.start.onclick = () => start();
-document.form_main.pause.onclick = () => pause();
-document.form_main.reset.onclick = () => reset();
-
-function start() {
-  pause();
-  cron = setInterval(() => { timer(); }, 10);
-}
-
-function pause() {
-  clearInterval(cron);
-}
-
-function reset() {
-  hour = 0;
-  minute = 0;
-  second = 0;
-  millisecond = 0;
-  document.getElementById('hour').innerText = '00';
-  document.getElementById('minute').innerText = '00';
-  document.getElementById('second').innerText = '00';
-  document.getElementById('millisecond').innerText = '000';
-}
-
-function timer() {
-  if ((millisecond += 10) == 1000) {
-    millisecond = 0;
-    second++;
-  }
-  if (second == 60) {
-    second = 0;
-    minute++;
-  }
-  if (minute == 60) {
-    minute = 0;
-    hour++;
-  }
-  document.getElementById('hour').innerText = returnData(hour);
-  document.getElementById('minute').innerText = returnData(minute);
-  document.getElementById('second').innerText = returnData(second);
-  document.getElementById('millisecond').innerText = returnData(millisecond);
-}
-
-function returnData(input) {
-  return input >= 10 ? input : `0${input}`
-}
