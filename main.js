db153dd3b2ddf0508be537efd23b3162936b4aa5
Meghan Carter <msc2010@yahoo.com>
2023-07-22 19:17:42 -0500

    update fetch

--- main.js
+++ main.js
@@ -1,39 +1,40 @@
 //two event listeners w/ unique callback function
 
 //Your project must implement at least one instance of array iteration using available array methods (map, forEach, filter, etc). Interacting with your API data in some way should present an opportunity to use array iteration.
+ 
 
-fetch("http://localhost:3000/emotions")
+  document.addEventListener("DOMContentLoaded", () => {
+   
+  fetch("http://localhost:3000/emotions")
   .then (response => response.json())
-  .then (function(data){
-    console.log(data)
-  }) 
-//   method:'POST',
-//   headers: {
-//     'Content-Type': 'application/json'
-//   },
-//   body: JSON.stringify ({
-//       name: 'emotions'
-//   })
-// }
-// .then(function(response){
-//   return response.json();})
-//   .then(function(data){
-//     //data from DOM
-// })
+  .then (emotions => renderEmotions(emotions))
+ 
 
+  function renderEmotions(emotions) {
+    emotions.forEach((emotion) => {
+      const emotionList = document.getElementById("container")
+      document.getElementById("name")
+      document.getElementById("image")
+
+      const button = document.createElement("button")
+      button.setAttribute("id", `${emotions.id}`)
+
+      container.append("emotionList")
+    })}
+  })
 
 //submit forms
 
-let submitForm = document.querySelector("submitForm");
-let submitButton = document.querySelector("SubmitButton");
+// let submitForm = document.querySelector("submitForm");
+// let submitButton = document.querySelector("SubmitButton");
 
-submitButton.addEventListener("click", function(e){
-  e.preventDefault();
-  alert ("SUBMITTED!");
-});
+// submitButton.addEventListener("click", function(e){
+//   e.preventDefault();
+//   alert ("SUBMITTED!");
+// });
 
-submitForm.addEventListener("submit", function(e) {
-  e.preventDefault();
-})
+// submitForm.addEventListener("submit", function(e) {
+//   e.preventDefault();
+// })
 
 
